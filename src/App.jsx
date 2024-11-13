e2549a9b51c5bd40b5375962e39ebce5da10e0a7
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-11-13 12:27:13 +0200

    Refactoring Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 // https://fullstackopen.com/en/part2/rendering_a_collection_modules
 
-const App = (props) => {
-  const { notes } = props;
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
-      //This solution is not recommended and can create undesired problems even if it seems to be working just fine.
-      <ul>
-        {notes.map((note, i) => (
-          <li key={i}>{note.content}</li>
-        ))}
-      </ul>
-      {/*  This solution is fine
       <ul>
         {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+          <Note key={note.id} note={note} />
         ))}
       </ul>
-      */}
     </div>
   );
 };

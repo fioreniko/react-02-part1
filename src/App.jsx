5f0d7fbcc106e6550d9b0759f115496ae879b532
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-11-13 11:00:56 +0200

    Rendering Collections- Key - Map

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// https://fullstackopen.com/en/part2/rendering_a_collection_modules
+
+const App = (props) => {
+  const { notes } = props;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
